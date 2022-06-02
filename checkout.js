eb4716f851dc6d3382ff6867287ccb3ccdf12f29
RynDaniel <ryan@clarusway.com>
2022-06-02 21:20:32 +0300

    first lesson commit

--- checkout.js
+++ checkout.js
@@ -1,1 +1,33 @@
+const taxRate = 0.18;
+const shippingPrice = 15.0;
+
+window.addEventListener("load", ()=>{
+    //set item to LocalStorage
+    localStorage.setItem("taxRate", taxRate);
+    localStorage.setItem("shippingPrice", shippingPrice);
+
+     //set item to SessionStorage
+    //  sessionStorage.setItem("taxRate", taxRate);
+    //  sessionStorage.setItem("shippingPrice", shippingPrice);
+
+});
+
+//capturing method
+let productsDiv = document.querySelector(".products");
+productsDiv.addEventListener("click", (event)=>{
+    if (event.target.className == "minus") {
+        event.target.nextElementSibling.innerText--;
+        // console.log("minus button clicked");
+    }
+    else if(event.target.classList.contains("plus")){
+        event.target.previousElementSibling.innerText++;
+        // console.log("plus button clicked");
+    }
+    else if(event.target.classList.contains("remove-product")){
+        // console.log("remove button clicked");
+    }
+    else{
+        // console.log("other element clicked");
+    }
+});
 
