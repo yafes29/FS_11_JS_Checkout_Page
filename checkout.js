e0a9493dee13a791ae25451c3e03b5ee8cd460ed
RynDaniel <ryan@clarusway.com>
2022-06-02 23:01:24 +0300

    hocam bitir artık commit

--- checkout.js
+++ checkout.js
@@ -1,10 +1,11 @@
-const taxRate = 0.18;
-const shippingPrice = 15.0;
+// const taxRate = 0.18;
+// const shippingPrice = 15.0;
 
 window.addEventListener("load", ()=>{
+    calculateCartTotal();
     //set item to LocalStorage
-    localStorage.setItem("taxRate", taxRate);
-    localStorage.setItem("shippingPrice", shippingPrice);
+    // localStorage.setItem("taxRate", taxRate);
+    // localStorage.setItem("shippingPrice", shippingPrice);
 
      //set item to SessionStorage
     //  sessionStorage.setItem("taxRate", taxRate);
@@ -38,8 +39,11 @@ productsDiv.addEventListener("click", (event)=>{
         // console.log("plus button clicked");
     }
     else if(event.target.classList.contains("remove-product")){
-        event.target.parentElement.parentElement.parentElement.remove();
-        calculateCartTotal();
+        if (confirm("Product will be deleted?")) {
+            event.target.parentElement.parentElement.parentElement.remove();
+            calculateCartTotal();
+        }
+      
         // console.log("remove button clicked");
     }
     else{
@@ -51,14 +55,39 @@ productsDiv.addEventListener("click", (event)=>{
 //calculate cart and product totals
 const calculateProductAndCartTotal = (productInfoDiv) =>{
 //product calculation
-let quantity =
-let price =
+// console.log(productInfoDiv);
+let price = productInfoDiv.querySelector("strong").innerText;
+let quantity = productInfoDiv.querySelector("#product-quantity").innerText;
+let productTotalDiv = productInfoDiv.querySelector(".product-line-price");
+productTotalDiv.innerText = (price * quantity).toFixed(2);
 //cart calculation
 calculateCartTotal();
 }
 
 //calculate cart totals
 const calculateCartTotal = () =>{
+    //nodeList Div
+    let productsTotalPriceDivs = document.querySelectorAll(".product-line-price");
+    // console.log(productsTotalPriceDivs);
+    let subtotal = 0;
+    productsTotalPriceDivs.forEach(eachProductTotalDiv=>{
+        subtotal += parseFloat(eachProductTotalDiv.innerText)
+    });
+    console.log(subtotal);
+    let taxPrice = subtotal * localStorage.getItem("taxRate");
+    console.log(taxPrice);
+
+    let shippingPrice = (subtotal > 0 ? parseFloat(localStorage.getItem("shippingPrice")) : 0);
+
+    let cartTotal = subtotal + taxPrice + shippingPrice;
+
+    document.querySelector("#cart-subtotal p:nth-child(2)").innerText = subtotal.toFixed(2);
+
+    document.querySelector("#cart-tax p:nth-child(2)").innerText = taxPrice.toFixed(2);
+    
+    document.querySelector("#cart-shipping p:nth-child(2)").innerText = shippingPrice.toFixed(2);
+
+    document.querySelector("#cart-total").lastElementChild.innerText = cartTotal.toFixed(2);
 
 }
 
